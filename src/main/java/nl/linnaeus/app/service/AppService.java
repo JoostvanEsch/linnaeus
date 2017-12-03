50b4f667026c4d4087bd3a3cad31a60648fe8e50
JoostvanEsch <joostvanesch@hotmail.com>
2017-12-03 20:35:30 +0100

    merge conflicts fixed

--- src/main/java/nl/linnaeus/app/service/AppService.java
+++ src/main/java/nl/linnaeus/app/service/AppService.java
@@ -1,6 +1,5 @@
 package nl.linnaeus.app.service;
 
-<<<<<<< HEAD
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,17 +8,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 import nl.linnaeus.app.model.User;
 
-=======
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
->>>>>>> 1a27f7c7083a9fadba709d280860a559dff53d4f
-
 @Service
 @Transactional
 public class AppService {
 	
-<<<<<<< HEAD
     @Autowired
     UserRepository userRepository;
     
@@ -30,8 +22,5 @@ public class AppService {
 	public ArrayList<User> getUsersFromDatabase() {
 		return (ArrayList<User>) userRepository.findAll();
 	}
-	
-=======
->>>>>>> 1a27f7c7083a9fadba709d280860a559dff53d4f
 
 }
